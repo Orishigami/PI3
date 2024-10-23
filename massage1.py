780e941841f8bbf3b6f7b33eed06c6590ac757fa
Orishigami <641463017@crru.ac.th>
2024-10-23 20:30:17 +0700

    v8

--- massage1.py
+++ massage1.py
@@ -1,7 +1,7 @@
 import socket
 
 # ตั้งค่าการเชื่อมต่อ
-host = 'IP_ADDRESS_OF_SERVER'  # IP Address ของโน๊ตบุ๊คที่เป็น Server
+host = '10.10.38.63'  # IP Address ของโน๊ตบุ๊คที่เป็น Server
 port = 12345
 
 # สร้าง socket object
