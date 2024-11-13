fa543a8fb04a778838582d64b6d201fe1df7dae4
Orishigami <641463017@crru.ac.th>
2024-11-13 14:11:36 +0700

    number1

--- Traffic_D.py
+++ Traffic_D.py
@@ -13,10 +13,10 @@ class TrafficLightController:
         # การกำหนดค่าเริ่มต้นสำหรับส่งไปยัง server
         self.config = {
             "camera_ids": {
-                "A": 2,
-                "B": 0,
+                "A": 0,
+                "B": 4,
                 "C": 1,
-                "D": 4
+                "D": 2
             },
             "direction_names": {
                 "A": "North",
